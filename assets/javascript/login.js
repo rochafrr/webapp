e770729dca7bc757a3fb3c342f44012c0f600fee
Fábio Rodrigues <binho_mal@hotmail.com>
2022-12-15 20:40:17 -0300

    first commit

---
+++ assets/javascript/login.js
@@ -0,0 +1,19 @@
+$('#login').on('submit', fazerLogin);
+
+function fazerLogin(evento) {
+    evento.preventDefault();
+
+    $.ajax({
+        url: "/login",
+        method: "POST",
+        data: {
+            email:$('#email').val(),
+            senha:$('#senha').val()
+        }
+
+    }).done(function(){
+        window.location = "/home";
+    }).fail(function(){
+        Swal.fire("Vish!", "Usuário ou senha incorretos!", "error");
+    })
+}
